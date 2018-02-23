9f8ad2eae06632650950cac09ba4725749684f69
mariia_suvalova <mariia_suvalova@epam.com>
2018-02-23 16:29:06 +0200

    Add sorting for products.

--- CorePlusAngular/SweetShop.WEB/ClientApp/app/products/product-list.component.ts
+++ CorePlusAngular/SweetShop.WEB/ClientApp/app/products/product-list.component.ts
@@ -9,6 +9,7 @@ import { Product } from './product';
 export class ProductListComponent implements OnInit {
 
     products: Product[];
+    sortBy: string;
     constructor(private dataService: DataService) { }
 
     ngOnInit() {
@@ -22,4 +23,55 @@ export class ProductListComponent implements OnInit {
     delete(id: number) {
         this.dataService.deleteProduct(id).subscribe(data => this.load());
     }
+
+    sortByName() {
+        this.products.sort(sortByNameExpression)
+        this.sortBy='name'
+    }
+
+    sortByCompany() {
+        this.products.sort(sortByCompanyExpression)
+        this.sortBy='company'
+    }
+
+    sortByPrice() {
+        this.products.sort(sortByPriceExpression)
+        this.sortBy='price'
+    }
+}
+
+function sortByNameExpression(s1: Product, s2: Product) {
+    if (s1.name > s2.name) {
+        return 1;
+    }
+
+    if (s1.name < s2.name) {
+        return -1;
+    }
+
+    return 0;
+}
+
+function sortByCompanyExpression(s1: Product, s2: Product) {
+    if (s1.company > s2.company) {
+        return 1;
+    }
+
+    if (s1.company > s2.company) {
+        return -1;
+    }
+
+    return 0;
+}
+
+function sortByPriceExpression(s1: Product, s2: Product) {
+    if (s1.price > s2.price) {
+        return 1;
+    }
+
+    if (s1.price > s2.price) {
+        return -1;
+    }
+
+    return 0;
 }
