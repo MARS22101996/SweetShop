4d3a0c2a0b469e3e9ef5758b5970ceaa3bab15c0
mariia_suvalova <mariia_suvalova@epam.com>
2018-04-10 15:46:45 +0300

    Added jsPDF to solution.

--- CorePlusAngular/SweetShop.WEB/ClientApp/app/basket/basket.component.ts
+++ CorePlusAngular/SweetShop.WEB/ClientApp/app/basket/basket.component.ts
@@ -7,31 +7,62 @@ import { DatePipe } from '@angular/common';
 import { OrderDetails } from "../shared/models/order.detail";
 
 @Component({
-    templateUrl: './basket.component.html'
+  templateUrl: './basket.component.html'
 })
 export class BasketComponent implements OnInit {
-    order: Order;
-    orderDetails: OrderDetails[];
-    constructor(private router: Router, private basketService: BasketService) { }
+  order: Order;
+  orderDetails: OrderDetails[];
+  constructor(private router: Router, private basketService: BasketService) { }
 
-    ngOnInit() {
-        this.load();
-    }
+  ngOnInit() {
+    this.load();
+  }
 
-    load() {
-        this.basketService.getBasket().subscribe((data: Order) => {
-            this.order = data;
-            this.orderDetails = data.orderDetails
-        });
-    }
+  load() {
+    this.basketService.getBasket().subscribe((data: Order) => {
+      this.order = data;
+      this.orderDetails = data.orderDetails
+    });
+  }
 
-    delete(id: number) {
-        this.basketService.delete(id).subscribe(data => {
-            this.load();
-        });
-    }
+  delete(id: number) {
+    this.basketService.delete(id).subscribe(data => {
+      this.load();
+    });
+  }
 
-    edit(orderDetails: OrderDetails) {
-        this.basketService.updateOrderDetail(orderDetails).subscribe(data => this.load());
-    }
+  edit(orderDetails: OrderDetails) {
+    this.basketService.updateOrderDetail(orderDetails).subscribe(data => this.load());
+  }
+
+  convert() {
+    let jsPDF = require('jspdf');
+    require('jspdf-autotable');
+    
+    let pdf = new jsPDF('l', 'pt', 'a4');
+
+    var orderDate = new Date(this.order.date);
+    pdf.text(35, 25, 'Order - ' + orderDate.toLocaleDateString());
+
+    var source = document.getElementById("basic-table");
+    var res = pdf.autoTableHtmlToJson(source);
+
+    pdf.autoTable(res.columns, res.data,
+      {
+        margin: { top: 80 },
+        theme: 'grid',
+        startY: 60,
+        styles: {
+          lineWidth: 0.01,
+          lineColor: 0,
+          fillStyle: 'DF',
+          halign: 'center',
+          valign: 'middle',
+          columnWidth: 'auto',
+          overflow: 'linebreak'
+        }
+      });
+
+    pdf.save("Basket.pdf");
+  }
 }
